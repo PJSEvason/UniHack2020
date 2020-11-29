d7ede51608c89c1a570a8267c8ad7909ac056266
Yoana Kaneva <yoana.kaneva@student.manchester.ac.uk>
2020-11-29 01:42:42 +0000

    Index page improved css

--- pyLearn/media/userID_editor.py
+++ pyLearn/media/userID_editor.py
@@ -1,2 +1,4 @@
-print(50)
-print("aaaaa")
+rainbow_colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+print(rainbow_colors[4])
+rainbow_colors.append("pink")
+print(rainbow_colors)
